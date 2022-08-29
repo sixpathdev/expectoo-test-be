e1c81516e9fb2289fb248a44d708586908544c4a
Miracle Gabriel <sixpathdev@gmail.com>
2022-08-29 13:38:29 +0100

    postgres remote config

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,19 @@ const dbconnection =
         dialect: 'postgres',
       })
     : new Sequelize(
-        'postgres://ufhfrulnjgswuh:b1ef7e66700819545980df0e08c3d5b25cdaf12d630bca22b77c92c5caf0e934@ec2-52-3-200-138.compute-1.amazonaws.com:5432/d9edm6j58p56l8'
-      )
+  'd9edm6j58p56l8',
+  'ufhfrulnjgswuh',
+  'b1ef7e66700819545980df0e08c3d5b25cdaf12d630bca22b77c92c5caf0e934',
+  {
+    host: 'ec2-52-3-200-138.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
+  }
+)
 
 module.exports = dbconnection
