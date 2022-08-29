d000a87e80d19aa100d201b9b13c70431ec496ff
Miracle Gabriel <sixpathdev@gmail.com>
2022-08-29 13:41:44 +0100

    postgres remote config

--- config/connection.js
+++ config/connection.js
@@ -14,8 +14,8 @@ const dbconnection =
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false, // This line will fix new error
+        require: true,
+        rejectUnauthorized: false,
       },
     },
   }
